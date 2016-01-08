e9a7abb3cfa160d20e35c8a06d5cf65c81377289
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-01-08 16:04:59 -0600

    Change structure to py.test

--- test/from_bytes_c.py
+++ test/from_bytes_c.py
@@ -49,26 +49,23 @@ avroschema = """ {"type": "record",
 """
 
 
-class TestFromBytes(unittest.TestCase):
-    def test_from_bytes(self):
-        tmpdir = tempfile.gettempdir()
-      #   fpath = os.path.join(tmpdir, 'from_bytes_data.avro')
-        fpath = os.path.join('from_bytes_data.avro')
-        writer = cyavro.AvroWriter(fpath, 'null', avroschema)
+def test_from_bytes():
+    tmpdir = tempfile.gettempdir()
+    fpath = os.path.join('from_bytes_data.avro')
+    writer = cyavro.AvroWriter(fpath, 'null', avroschema)
 
-        ids = np.random.randint(100, size=10)
-        ids = np.arange(10)
-        names = pdt.rands_array(10, 10)
-        df_write = pd.DataFrame({"id": ids, "name": names})
-        df_write = cyavro.prepare_pandas_df_for_write(df_write, avroschema, copy=False)
-        print df_write
-        writer.write(df_write)
-        writer.close()
+    ids = np.random.randint(100, size=10)
+    ids = np.arange(10)
+    names = pdt.rands_array(10, 10)
+    df_write = pd.DataFrame({"id": ids, "name": names})
+    df_write = cyavro.prepare_pandas_df_for_write(df_write, avroschema, copy=False)
 
-        reader = from_bytes_c_helper.get_reader(fpath)
-        reader.init_buffers()
-        df_read = pd.DataFrame(reader.read_chunk())
-        print df_read
-        print reader.read_chunk()
-      #   pdt.assert_frame_equal(df_write, df_read)
-        reader.close()
+    writer.write(df_write)
+    writer.close()
+
+    reader = from_bytes_c_helper.get_reader(fpath)
+    reader.init_buffers()
+    df_read = pd.DataFrame(reader.read_chunk())
+
+    pdt.assert_frame_equal(df_write, df_read)
+    reader.close()
