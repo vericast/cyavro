ddca2e955fafbbd4afc014a404d3dfd4bfc8aeb8
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-08-20 19:10:49 -0400

    switch to setuptools

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 import numpy as np
